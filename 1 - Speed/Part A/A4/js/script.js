c5194471ac460726b62e7a7aef2ef9dbca85ef5e
Charlie Brewis <charliebrewis7@gmail.com>
2024-03-24 12:11:10 +0000

    A4 done

--- 1 - Speed/Part A/A4/js/script.js
+++ 1 - Speed/Part A/A4/js/script.js
@@ -1,1 +1,26 @@
 //Put your code here
+
+const face = document.querySelector(".face");
+const happyButton = document.querySelector("#happy");
+const neutralButton = document.querySelector("#neutral");
+const sadButton = document.querySelector("#sad");
+
+happyButton.addEventListener("click", () => {
+    animateFace("green");
+    face.textContent = ":)";
+});
+neutralButton.addEventListener("click", () => {
+    animateFace("yellow");
+    face.textContent = ":|";
+});
+sadButton.addEventListener("click", () => {
+    animateFace("red");
+    face.textContent = ":(";
+});
+
+function animateFace(to) {
+    face.animate([{ backgroundColor: face.style.backgroundColor, backgroundColor: to }], {
+        duration: 500,
+        fill: "forwards", // Keeps the final animation state
+    });
+}
