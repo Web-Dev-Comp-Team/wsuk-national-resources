4b2950338a09b09ad137bcd23f9a4b02ad3282fa
Charlie Brewis <charliebrewis7@gmail.com>
2024-03-25 14:50:00 +0000

    Made a start to B3

--- 1 - Speed/Part B/B3/js/script.js
+++ 1 - Speed/Part B/B3/js/script.js
@@ -1,2 +1,17 @@
 //Put your code here
 
+const colorBlock = document.querySelector(".color-block");
+const redSlider = document.querySelector("#red");
+const greenSlider = document.querySelector("#green");
+const blueSlider = document.querySelector("#blue");
+
+var redValue = redSlider.value;
+
+redSlider.addEventListener("change", () => {
+    redValue = redSlider.value;
+    updateColorBlock;
+});
+
+function updateColorBlock() {
+    colorBlock.style.backgroundColor = redValue;
+}
