2f426f002da8ca8e4edda964eead4e84ba820d7a
Derek Walton <derekwalton19@gmail.com>
2024-04-29 13:38:23 +0100

    finished speed part A

--- 1 - Speed/Part A/A4/js/script.js
+++ 1 - Speed/Part A/A4/js/script.js
@@ -1,1 +1,31 @@
-//Put your code here
+
+const smileyButton = document.querySelector('#happy');
+const averageButton = document.querySelector('#average');
+const sadButton = document.querySelector('#sad');
+
+const face = document.getElementById('face');
+const faceText = document.querySelector('.mood');
+
+smileyButton.onclick = happy;
+averageButton.onclick = average;
+sadButton.onclick = sad;
+
+function happy(){
+    changeFaceText(':)');
+    face.style.backgroundColor = '#a9db71';
+}
+
+function average(){
+    changeFaceText(':|');
+    face.style.backgroundColor = '#eed16b';
+}
+
+function sad(){
+    changeFaceText(':(');
+    face.style.backgroundColor = '#ee9395';
+}
+
+function changeFaceText(newFace){
+    face.style.transitionDuration = '0.5s';
+    faceText.innerHTML = newFace;
+}
