ef8b43c6733bd0903e20525ebf239e9ff7a1492b
Derek Walton <derekwalton19@gmail.com>
2024-05-03 12:05:01 +0100

    finishing smiley faces

--- 1 - Speed/Part C/C1/js/script.js
+++ 1 - Speed/Part C/C1/js/script.js
@@ -1,1 +1,14 @@
-//Put your code here
+const leftColourInput = document.getElementById('color-input-left');
+const rightColourInput = document.getElementById('color-input-right');
+
+const container = document.getElementById('container');
+
+// leftColourInput.onchange = changeGradient;
+
+leftColourInput = "'blue'"
+
+changeGradient();
+
+function changeGradient(){
+    container.style.background = 'linear-gradient(to right, ' + leftColourInput + ', ' + rightColourInput + ')';
+}
