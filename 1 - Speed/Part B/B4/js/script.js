8e1aaa6008bdf326005b4686b17792d4ef0bb808
Charlie Brewis <charliebrewis7@gmail.com>
2024-05-03 12:56:47 +0100

    B4 partial attempt

--- 1 - Speed/Part B/B4/js/script.js
+++ 1 - Speed/Part B/B4/js/script.js
@@ -1,1 +1,22 @@
 //Put your code here
+
+const imageOverlay = document.querySelector(".image-overlay");
+const imageContainer = document.querySelector(".image-container");
+// const main = document.getElementsByClassName('main');
+
+function handleMouseMove(event) {
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+    // console.log("Mouse Position:", mouseX, mouseY);
+    if (
+        mouseX >= imageContainer.offsetLeft &&
+        mouseX <= imageContainer.offsetLeft + imageContainer.offsetWidth &&
+        mouseY >= imageContainer.offsetTop &&
+        mouseY <= imageContainer.offsetTop + imageContainer.offsetHeight
+    ) {
+        // imageOverlay.style.offsetWidth = mouseX;
+        imageOverlay.style.width = mouseX - imageContainer.offsetLeft + "px";
+        console.log(mouseX - imageContainer.offsetLeft + "px");
+    }
+}
+document.addEventListener("mousemove", handleMouseMove);
