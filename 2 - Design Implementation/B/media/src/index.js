af6afdde2d37afd801781fdebc96155e2ae2460d
Derek Walton <derekwalton19@gmail.com>
2024-06-05 21:27:27 +0100

    finished from 900px to 1400px

--- 2 - Design Implementation/B/media/src/index.js
+++ 2 - Design Implementation/B/media/src/index.js
@@ -5,26 +5,37 @@ const collaborate = document.querySelector('.collaborate-nav');
 const news = document.querySelector('.news-nav');
 const about = document.querySelector('.about-nav');
 
+const sidebarButton = document.querySelector('#sidebar-button');
+
+const secondNavItems = document.querySelectorAll('.second-nav-item');
+
 const list = [study, student, research, collaborate, news, about];
 
-list.forEach(element => {
-  element.addEventListener('mouseenter', displayThirdNav);
-  // element.addEventListener('mouseleave', displayNone);
+sidebarButton.addEventListener('click', () => {
+  secondNavItems.forEach(navItem => {
+    console.log(navItem)
+    navItem.style.display = 'flex';
+  });
 });
 
-function displayThirdNav(event) {
-  const classname = event.target.classList[2].split('-')[0];
-  const element = document.querySelector('#' + classname + '-third-nav');
-  element.style.display = 'flex';
-  element.addEventListener('mouseleave', displayNone);
-}
+// list.forEach(element => {
+//   element.addEventListener('mouseenter', displayThirdNav);
+//   // element.addEventListener('mouseleave', displayNone);
+// });
 
-// function displayNone1(event) {
+// function displayThirdNav(event) {
 //   const classname = event.target.classList[2].split('-')[0];
 //   const element = document.querySelector('#' + classname + '-third-nav');
-//   element.style.display = 'none';
+//   element.style.display = 'flex';
+//   element.addEventListener('mouseleave', displayNone);
 // }
 
-function displayNone(event) {
-  event.target.style.display = 'none';
-}
+// // function displayNone1(event) {
+// //   const classname = event.target.classList[2].split('-')[0];
+// //   const element = document.querySelector('#' + classname + '-third-nav');
+// //   element.style.display = 'none';
+// // }
+
+// function displayNone(event) {
+//   event.target.style.display = 'none';
+// }
