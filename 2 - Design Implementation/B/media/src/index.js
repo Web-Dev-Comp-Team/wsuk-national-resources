790c84cbf5dbcf7c8601506274e4ed1dc5be5fd7
Derek Walton <derekwalton19@gmail.com>
2024-06-05 17:39:13 +0100

    working-ish nav menu

--- 2 - Design Implementation/B/media/src/index.js
+++ 2 - Design Implementation/B/media/src/index.js
@@ -1,13 +1,30 @@
-// const affectedElements = document.querySelectorAll('.pre-load-transition');
+const study = document.querySelector('.study-nav');
+const student = document.querySelector('.student-nav');
+const research = document.querySelector('.research-nav');
+const collaborate = document.querySelector('.collaborate-nav');
+const news = document.querySelector('.news-nav');
+const about = document.querySelector('.about-nav');
 
-// function timeout() {
-//   setTimeout(loadTransitions, 1000);
+const list = [study, student, research, collaborate, news, about];
+
+list.forEach(element => {
+  element.addEventListener('mouseenter', displayThirdNav);
+  // element.addEventListener('mouseleave', displayNone);
+});
+
+function displayThirdNav(event) {
+  const classname = event.target.classList[2].split('-')[0];
+  const element = document.querySelector('#' + classname + '-third-nav');
+  element.style.display = 'flex';
+  element.addEventListener('mouseleave', displayNone);
+}
+
+// function displayNone1(event) {
+//   const classname = event.target.classList[2].split('-')[0];
+//   const element = document.querySelector('#' + classname + '-third-nav');
+//   element.style.display = 'none';
 // }
 
-// function loadTransitions() {
-//   affectedElements.forEach((element) => {
-//     element.classList.remove('pre-load-transition');
-//   });
-// }
-
-// document.addEventListener('DOMContentLoaded', timeout);
+function displayNone(event) {
+  event.target.style.display = 'none';
+}
