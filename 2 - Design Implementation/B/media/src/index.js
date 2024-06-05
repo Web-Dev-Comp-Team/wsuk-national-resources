b39fbc4620e446d9c80a0ee5833f06e2cb11f751
Derek Walton <derekwalton19@gmail.com>
2024-06-05 16:45:32 +0100

    working state

---
+++ 2 - Design Implementation/B/media/src/index.js
@@ -0,0 +1,13 @@
+// const affectedElements = document.querySelectorAll('.pre-load-transition');
+
+// function timeout() {
+//   setTimeout(loadTransitions, 1000);
+// }
+
+// function loadTransitions() {
+//   affectedElements.forEach((element) => {
+//     element.classList.remove('pre-load-transition');
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', timeout);
